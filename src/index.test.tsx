196ecc8a89281336918c41d26f6fec5243ce4741
Bnaya Zilberfarb <bnayaz@wix.com>
2021-10-12 12:39:09 +0300

    tests & dealing with multiple childrens

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,101 @@
-import { ExampleComponent } from '.'
+import * as React from 'react'
+import { Listener } from '.'
+import { act, render } from '@testing-library/react'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+export const fireEvent = (event: any) => {
+  const addEventListener = window.addEventListener as ReturnType<typeof jest.fn>
+  const props = addEventListener.mock.calls.find((x) => x?.[0] === 'message')
+  props?.[1]!(event)
+}
+
+describe('Listener', () => {
+  let addEventListener: any
+  beforeAll(() => {
+    addEventListener = window.addEventListener
+  })
+  afterAll(() => {
+    window.addEventListener = addEventListener
+  })
+  beforeEach(() => {
+    window.addEventListener = jest.fn()
+  })
+  it('passing the event type', () => {
+    const child = jest.fn().mockReturnValue(<></>)
+    render(
+      <Listener event='message'>
+        {(event) => {
+          return child(event)
+        }}
+      </Listener>
+    )
+    expect(window.addEventListener).toBeCalledWith(
+      'message',
+      expect.any(Function),
+      undefined
+    )
+  })
+  it('passing the options', () => {
+    const child = jest.fn().mockReturnValue(<></>)
+    const options = {
+      once: true,
+      passive: true
+    }
+    render(
+      <Listener event='message' options={options}>
+        {(event) => {
+          return child(event)
+        }}
+      </Listener>
+    )
+    expect(window.addEventListener).toBeCalledWith(
+      'message',
+      expect.any(Function),
+      options
+    )
+  })
+  it('passing an event', () => {
+    const child = jest.fn().mockReturnValue(<></>)
+    render(
+      <Listener event='message'>
+        {(event) => {
+          return child(event)
+        }}
+      </Listener>
+    )
+    act(() => fireEvent('mocked event'))
+    expect(child).toBeCalledWith('mocked event')
+  })
+  describe('identifier', () => {
+    it('passing the identifier', () => {
+      const child = jest.fn().mockReturnValue(<></>)
+      render(
+        <Listener
+          event='message'
+          identifier={(event) => event.includes('event')}
+        >
+          {(event) => {
+            return child(event)
+          }}
+        </Listener>
+      )
+      act(() => fireEvent('mocked event'))
+      expect(child).toBeCalledWith('mocked event')
+    })
+
+    it('filtered by the identifier', () => {
+      const child = jest.fn().mockReturnValue(<></>)
+      render(
+        <Listener
+          event='message'
+          identifier={(event) => event.includes('real')}
+        >
+          {(event) => {
+            return child(event)
+          }}
+        </Listener>
+      )
+      act(() => fireEvent('mocked event'))
+      expect(child).not.toBeCalledWith('mocked event')
+    })
   })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,44 @@
 import * as React from 'react'
-import styles from './styles.module.css'
 
-interface Props {
-  text: string
+interface Props<K extends keyof WindowEventMap> {
+  event: K
+  identifier?: (event: WindowEventMap[K]) => boolean
+  options?: boolean | AddEventListenerOptions
+  children: (
+    event: WindowEventMap[K]
+  ) => React.ReactNode | ((event: WindowEventMap[K]) => React.ReactNode)[]
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export function Listener<K extends keyof WindowEventMap>({
+  event,
+  identifier = () => true,
+  options,
+  children
+}: Props<K>) {
+  const [_event, setEvent] =
+    React.useState<WindowEventMap[keyof WindowEventMap]>()
+  const setListener = React.useCallback(() => {
+    window.addEventListener(
+      event,
+      (ev) => {
+        if (!identifier(ev)) {
+          return
+        }
+        setEvent(ev)
+      },
+      options
+    )
+  }, [event, options])
+  React.useLayoutEffect(() => {
+    setListener()
+  }, [event, options])
+  if (!_event) {
+    return null
+  }
+  if (!Array.isArray(children)) {
+    return children(_event as WindowEventMap[K])
+  }
+  return React.Children.map(children, (child) => {
+    child(_event as WindowEventMap[K])
+  })
 }
